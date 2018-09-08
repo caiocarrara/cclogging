519800615072825e300594fe46a45d962c126c6e
Caio Carrara <eu@caiocarrara.com.br>
2018-09-08 01:14:24 -0300

    Adds https site_url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://caiocarrara.com.br'
+SITEURL = 'https://caiocarrara.com.br'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
